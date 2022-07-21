7cfc55b406a01a31ab93c73308825b323400e63d
Nilender-Andrey <Nilenderav@mail.ru>
2022-07-21 17:16:19 +0400

    feat: add build v0.1

--- src/components/products-page/Brand-filter.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Filter from './filters/Filter';
-
-const BrandFilter = () => {
-  return <Filter title='Бренд'></Filter>;
-};
-
-export default BrandFilter;
--- src/utils/check-letter.ts
+++
@@ -1,3 +0,0 @@
-export default function checkLetter(str: string, symbol: string) {
-  return str.toLowerCase().includes(symbol);
-}
